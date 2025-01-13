be1ef9e04030f5e428d6aa030dca2640eda1d78e
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2025-01-13 17:05:29 +0100

    pro create

---
+++ src/main/java/dev/nawid/SpringRunnerz/SpringRunnerzApplication.java
@@ -0,0 +1,13 @@
+package dev.nawid.SpringRunnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRunnerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRunnerzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nawid/SpringRunnerz/SpringRunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nawid.SpringRunnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
